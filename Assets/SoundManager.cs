3697a602ea600f1bacb73ecd4aedfa8159b20804
DanteLiang <glaxierblest@gmail.com>
2021-12-16 12:31:15 -0500

    cinemachine

---
+++ Assets/SoundManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    
+    public AudioSource 
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
